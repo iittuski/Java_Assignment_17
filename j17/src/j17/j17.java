19b37ff33214f989f4410f1d49498445ad2da003
iittuski <iittuski@gmail.com>
2016-04-15 18:34:18 -0700

    change
    change

---
+++ j17/src/j17/j17.java
@@ -0,0 +1,16 @@
+package j17;
+
+public class j17 {
+	int calculatearearec(int x,int y){
+		int area = x*y;
+		return area;
+	}
+	int calculatearetri(int x,int y){
+		int area = (x*y)/2;
+		return area;
+	}
+	public static void main(String args){
+		
+		
+	}
+}
---
+++ j17/src/j17/rectangle.java
@@ -0,0 +1,10 @@
+package j17;
+
+public class rectangle extends j17{
+
+	public static void main(String args[]){
+		j17 j17 = new j17();
+		int area = j17.calculatearearec(10, 10);
+		System.out.println(area);
+	}
+}
